0f73b07c1b6ae26fd9b51106aaabe6122a1e5e18
kovacsgaborab <kovacsgaborab@gmail.com>
2020-09-16 08:35:05 +0200

    Add project files.

---
+++ Kiralynok/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+
+    class Tabla
+    {
+        private char[,] T = new char[8, 8];
+
+        private char UresCella;
+        private int UresOszlopokSzama;
+        private int UresSorokSzama;
+
+        private static void Elhelyez()
+        {
+            
+        }
+        private static void FajlbaIr()
+        {
+            
+        }
+        private static void Megjelenit()
+        {
+
+        }
+        public Tabla(char ch)
+        {
+
+        }
+        private int UresOszlop()
+        {
+
+        }
+        private int UresSor()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
